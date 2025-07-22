351abb1e2cfe894a77cd68e0572554e8fc37a1e9
niaina <manohitiana@gmail.com>
2025-07-22 19:57:29 +0300

    Update config.py

--- app/config.py
+++ app/config.py
@@ -2,6 +2,6 @@ import os
 
 class Config:
     SECRET_KEY = '444f220181a12acbd8461f8314364b48adb996ba07c052f3'
-    MONGO_URI = 'mongodb://127.0.0.1:27017/Guichet'
+    MONGO_URI = mongodb+srv://manohitiana:hcedEcbvUBDBzusO@cluster0.f6gtwfk.mongodb.net/Gest_guichet?retryWrites=true&w=majority&appName=mongosh
     
     UPLOAD_FOLDER = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'static/uploads')
